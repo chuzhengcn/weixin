5c9db9ebee4ee27ef0c12830f01cb92489eea70a
chuzheng <chuzheng@gmail.com>
2013-10-08 15:39:55 +0800

    weixin validate signature

--- routes/msg.js
+++ routes/msg.js
@@ -6,7 +6,7 @@ exports.token = function (req, res) {
     var shasum = crypto.createHash('sha1'),
         signature           = req.query.signature,
         echostr             = req.query.echostr,
-        unencrypted_params  = [Token, req.query.timestamp, req.query.nonce].sort().join(),
+        unencrypted_params  = [Token, req.query.timestamp, req.query.nonce].sort().join().replace(/,/g, ''),
         encrypted_str       = '';
 
     shasum.update(unencrypted_params) 
